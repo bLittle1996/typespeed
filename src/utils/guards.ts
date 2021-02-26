bd17bb3d14a1d279bbe831d38fc31a2b2a01ca20
Ben Littleton <b.little1996@gmail.com>
2021-02-26 17:53:48 -0500

    Added a isString guard.

--- src/utils/guards.ts
+++ src/utils/guards.ts
@@ -1,3 +1,7 @@
 export const isDefined = <T>(thing: T): thing is Exclude<T, undefined> => {
   return typeof thing !== "undefined";
 };
+
+export const isString = (thing: unknown): thing is string => {
+  return typeof thing === "string";
+};
