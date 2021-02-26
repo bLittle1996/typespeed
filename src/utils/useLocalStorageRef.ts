a773c318ddf556dfe8f3eb97aa3aaa198491f320
Ben Littleton <b.little1996@gmail.com>
2021-02-26 17:58:41 -0500

    Updated some comments

--- src/utils/useLocalStorageRef.ts
+++ src/utils/useLocalStorageRef.ts
@@ -2,7 +2,7 @@ import { Ref, ref, UnwrapRef, watch } from "vue";
 import { isDefined, isString } from "./guards";
 
 /**
- * Writes to local storage in a predictable, parsabled format for later retrieval.
+ * Writes to local storage in a predictable, parsable format for later retrieval.
  * @param key
  * @param value
  */
@@ -18,6 +18,8 @@ const getItem = (key: string) => {
   const localStorageValue = localStorage.getItem(key);
 
   if (isString(localStorageValue)) {
+    // This may fail (if the key is a plain string instead of an object, for example)
+    // So if that happens, we should just return nothing since the format is invalid.
     try {
       return JSON.parse(localStorageValue).data;
     } catch {
@@ -32,7 +34,7 @@ export const useLocalStorageRef = <T>(
   key: string,
   fallbackValue?: T
 ): Ref<UnwrapRef<T> | undefined> => {
-  // Try to initialize with the value in storage, otherwise use the initial value.
+  // Try to initialize with the value in storage, otherwise use the fallback value.
   const value = ref<T | undefined>(getItem(key) ?? fallbackValue);
 
   /**
